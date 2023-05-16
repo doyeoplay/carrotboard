eec7090de0badb5d0cabc153467ed92cbbd7359d
doyeop <doyeoplay@gmail.com>
2023-05-16 17:01:16 +0900

    first commit

---
+++ class/pages/freeboard/postreg.js
@@ -0,0 +1,11 @@
+import { MainPage,Title,ContTit } from '../../styles/emotion'
+
+export default function PostReg(){
+
+    return(
+        <MainPage>
+            <Title>게시물 등록</Title>
+            <ContTit>작성자</ContTit><ContTit>비밀번호</ContTit>
+        </MainPage>
+    )
+}
